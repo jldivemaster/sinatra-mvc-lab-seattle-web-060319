87d8958ebf1bf432d26d9aef992db1fef5639ca0
Jason Leach <jlspursfan@gmail.com>
2019-06-26 04:42:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
 
   get '/' do
-    erb :'views/user_input'
+    erb :views/user_input
   end
 
   post '/piglatinize' do
     @output = PigLatinizer.new(params[:user_input])
-    erb :'views/output'
+    erb :views/output
   end
 end
